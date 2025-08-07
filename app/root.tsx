b40cd8cafc50103c738cc4cbdf6b5eaa061ce24f
DevTiger <77429319+ohwoo-kwon@users.noreply.github.com>
2025-08-07 16:27:04 +0900

    google adsense meta

--- app/root.tsx
+++ app/root.tsx
@@ -52,8 +52,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-adsense-account" content="ca-pub-1792324013654410" />
         <Meta />
         <Links />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1792324013654410"
+          crossorigin="anonymous"
+        />
       </head>
       <body>
         {children}
