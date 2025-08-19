d7f993dacce092297ed14a7a29c1ededc1aa78a7
ohwoo_kwon <kwonow1@gmail.com>
2025-08-19 20:57:29 +0900

    내림차순 적용

--- app/features/users/queries.ts
+++ app/features/users/queries.ts
@@ -51,7 +51,7 @@ export const getChats = async (
     .select("*")
     .eq("profile_id", userId)
     .neq("other_profile_id", userId)
-    .order("last_message_time");
+    .order("last_message_time", { ascending: false });
 
   if (error) throw error;
 
